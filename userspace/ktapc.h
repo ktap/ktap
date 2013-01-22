08226e46d92fc0092353dd13252e4269694b9fa4
zhangwei(Jovi) <jovi.zhangwei@huawei.com>
2013-01-22 13:46:57 +0800

    code indent unify
    Signed-off-by: zhangwei(Jovi) <jovi.zhangwei@huawei.com>

--- userspace/ktapc.h
+++ userspace/ktapc.h
@@ -65,41 +65,40 @@
 
 
 /*
- * * WARNING: if you change the order of this enumeration,
- * * grep "ORDER RESERVED"
- * */
+ * WARNING: if you change the order of this enumeration,
+ * grep "ORDER RESERVED"
+ */
 enum RESERVED {
-  /* terminal symbols denoted by reserved words */
-  TK_AND = FIRST_RESERVED, TK_BREAK,
-  TK_DO, TK_ELSE, TK_ELSEIF, TK_END, TK_FALSE, TK_FOR, TK_FUNCTION,
-  TK_GOTO, TK_IF, TK_IN, TK_LOCAL, TK_NIL, TK_NOT, TK_OR, TK_REPEAT,
-  TK_RETURN, TK_THEN, TK_TRUE, TK_UNTIL, TK_WHILE,
-  /* other terminal symbols */
-  TK_CONCAT, TK_DOTS, TK_EQ, TK_GE, TK_LE, TK_NE, TK_DBCOLON, TK_EOS,
-  TK_NUMBER, TK_NAME, TK_STRING
+	/* terminal symbols denoted by reserved words */
+	TK_AND = FIRST_RESERVED, TK_BREAK,
+	TK_DO, TK_ELSE, TK_ELSEIF, TK_END, TK_FALSE, TK_FOR, TK_FUNCTION,
+	TK_GOTO, TK_IF, TK_IN, TK_LOCAL, TK_NIL, TK_NOT, TK_OR, TK_REPEAT,
+	TK_RETURN, TK_THEN, TK_TRUE, TK_UNTIL, TK_WHILE,
+	/* other terminal symbols */
+	TK_CONCAT, TK_DOTS, TK_EQ, TK_GE, TK_LE, TK_NE, TK_DBCOLON, TK_EOS,
+	TK_NUMBER, TK_NAME, TK_STRING
 };
 
 /* number of reserved words */
 #define NUM_RESERVED    ((int)(TK_WHILE-FIRST_RESERVED + 1))
 
-//#define EOZ     (-1)                    /* end of stream */
 #define EOZ     (0)                    /* end of stream */
 
 typedef union {
-  ktap_Number r;
-  Tstring *ts;
+	ktap_Number r;
+	Tstring *ts;
 } SemInfo;  /* semantics information */
 
 
 typedef struct Token {
-  int token;
-  SemInfo seminfo;
+	int token;
+	SemInfo seminfo;
 } Token;
 
 typedef struct Mbuffer {
-  char *buffer;
-  size_t n;
-  size_t buffsize;
+	char *buffer;
+	size_t n;
+	size_t buffsize;
 } Mbuffer;
 
 #define mbuff_init(buff)	((buff)->buffer = NULL, (buff)->buffsize = 0)
@@ -115,44 +114,45 @@ typedef struct Mbuffer {
 #define mbuff_free(buff)        mbuff_resize(buff, 0)
 
 
-/* state of the lexer plus state of the parser when shared by all
- *    functions */
+/*
+ * state of the lexer plus state of the parser when shared by all
+ * functions
+ */
 typedef struct LexState {
-  unsigned char *ptr; /* source file reading position */
-  int current;  /* current character (charint) */
-  int linenumber;  /* input line counter */
-  int lastline;  /* line of last token `consumed' */
-  Token t;  /* current token */
-  Token lookahead;  /* look ahead token */
-  struct FuncState *fs;  /* current function (parser) */
-  Mbuffer *buff;  /* buffer for tokens */
-  struct Dyndata *dyd;  /* dynamic structures used by the parser */
-  Tstring *source;  /* current source name */
-  Tstring *envn;  /* environment variable name */
-  char decpoint;  /* locale decimal point */
-  int nCcalls;
+	unsigned char *ptr; /* source file reading position */
+	int current;  /* current character (charint) */
+	int linenumber;  /* input line counter */
+	int lastline;  /* line of last token `consumed' */
+	Token t;  /* current token */
+	Token lookahead;  /* look ahead token */
+	struct FuncState *fs;  /* current function (parser) */
+	Mbuffer *buff;  /* buffer for tokens */
+	struct Dyndata *dyd;  /* dynamic structures used by the parser */
+	Tstring *source;  /* current source name */
+	Tstring *envn;  /* environment variable name */
+	char decpoint;  /* locale decimal point */
+	int nCcalls;
 } LexState;
 
 
 /*
- * ** Expression descriptor
- * */
-
+ * Expression descriptor
+ */
 typedef enum {
-  VVOID,        /* no value */
-  VNIL,
-  VTRUE,
-  VFALSE,
-  VK,           /* info = index of constant in `k' */
-  VKNUM,        /* nval = numerical value */
-  VNONRELOC,    /* info = result register */
-  VLOCAL,       /* info = local register */
-  VUPVAL,       /* info = index of upvalue in 'upvalues' */
-  VINDEXED,     /* t = table register/upvalue; idx = index R/K */
-  VJMP,         /* info = instruction pc */
-  VRELOCABLE,   /* info = instruction pc */
-  VCALL,        /* info = instruction pc */
-  VVARARG       /* info = instruction pc */
+	VVOID,        /* no value */
+	VNIL,
+	VTRUE,
+	VFALSE,
+	VK,           /* info = index of constant in `k' */
+	VKNUM,        /* nval = numerical value */
+	VNONRELOC,    /* info = result register */
+	VLOCAL,       /* info = local register */
+	VUPVAL,       /* info = index of upvalue in 'upvalues' */
+	VINDEXED,     /* t = table register/upvalue; idx = index R/K */
+	VJMP,         /* info = instruction pc */
+	VRELOCABLE,   /* info = instruction pc */
+	VCALL,        /* info = instruction pc */
+	VVARARG       /* info = instruction pc */
 } expkind;
 
 
@@ -160,52 +160,52 @@ typedef enum {
 #define vkisinreg(k)    ((k) == VNONRELOC || (k) == VLOCAL)
 
 typedef struct expdesc {
-  expkind k;
-  union {
-    struct {  /* for indexed variables (VINDEXED) */
-      short idx;  /* index (R/K) */
-      u8 t;  /* table (register or upvalue) */
-      u8 vt;  /* whether 't' is register (VLOCAL) or upvalue (VUPVAL) */
-    } ind;
-    int info;  /* for generic use */
-    ktap_Number nval;  /* for VKNUM */
-  } u;
-  int t;  /* patch list of `exit when true' */
-  int f;  /* patch list of `exit when false' */
+	expkind k;
+	union {
+		struct {  /* for indexed variables (VINDEXED) */
+			short idx;  /* index (R/K) */
+			u8 t;  /* table (register or upvalue) */
+			u8 vt;  /* whether 't' is register (VLOCAL) or upvalue (VUPVAL) */
+		} ind;
+		int info;  /* for generic use */
+		ktap_Number nval;  /* for VKNUM */
+	} u;
+	int t;  /* patch list of `exit when true' */
+	int f;  /* patch list of `exit when false' */
 } expdesc;
 
 
 typedef struct Vardesc {
-  short idx;  /* variable index in stack */
+	short idx;  /* variable index in stack */
 } Vardesc;
 
 
 /* description of pending goto statements and label statements */
 typedef struct Labeldesc {
-  Tstring *name;  /* label identifier */
-  int pc;  /* position in code */
-  int line;  /* line where it appeared */
-  u8 nactvar;  /* local level where it appears in current block */
+	Tstring *name;  /* label identifier */
+	int pc;  /* position in code */
+	int line;  /* line where it appeared */
+	u8 nactvar;  /* local level where it appears in current block */
 } Labeldesc;
 
 
 /* list of labels or gotos */
 typedef struct Labellist {
-  Labeldesc *arr;  /* array */
-  int n;  /* number of entries in use */
-  int size;  /* array size */
+	Labeldesc *arr;  /* array */
+	int n;  /* number of entries in use */
+	int size;  /* array size */
 } Labellist;
 
 
 /* dynamic structures used by the parser */
 typedef struct Dyndata {
-  struct {  /* list of active local variables */
-    Vardesc *arr;
-    int n;
-    int size;
-  } actvar;
-  Labellist gt;  /* list of pending gotos */
-  Labellist label;   /* list of active labels */
+	struct {  /* list of active local variables */
+		Vardesc *arr;
+		int n;
+		int size;
+	} actvar;
+	Labellist gt;  /* list of pending gotos */
+	Labellist label;   /* list of active labels */
 } Dyndata;
 
 
@@ -215,43 +215,43 @@ struct BlockCnt;  /* defined in lparser.c */
 
 /* state needed to generate code for a given function */
 typedef struct FuncState {
-  Proto *f;  /* current function header */
-  Table *h;  /* table to find (and reuse) elements in `k' */
-  struct FuncState *prev;  /* enclosing function */
-  struct LexState *ls;  /* lexical state */
-  struct BlockCnt *bl;  /* chain of current blocks */
-  int pc;  /* next position to code (equivalent to `ncode') */
-  int lasttarget;   /* 'label' of last 'jump label' */
-  int jpc;  /* list of pending jumps to `pc' */
-  int nk;  /* number of elements in `k' */
-  int np;  /* number of elements in `p' */
-  int firstlocal;  /* index of first local var (in Dyndata array) */
-  short nlocvars;  /* number of elements in 'f->locvars' */
-  u8 nactvar;  /* number of active local variables */
-  u8 nups;  /* number of upvalues */
-  u8 freereg;  /* first free register */
+	Proto *f;  /* current function header */
+	Table *h;  /* table to find (and reuse) elements in `k' */
+	struct FuncState *prev;  /* enclosing function */
+	struct LexState *ls;  /* lexical state */
+	struct BlockCnt *bl;  /* chain of current blocks */
+	int pc;  /* next position to code (equivalent to `ncode') */
+	int lasttarget;   /* 'label' of last 'jump label' */
+	int jpc;  /* list of pending jumps to `pc' */
+	int nk;  /* number of elements in `k' */
+	int np;  /* number of elements in `p' */
+	int firstlocal;  /* index of first local var (in Dyndata array) */
+	short nlocvars;  /* number of elements in 'f->locvars' */
+	u8 nactvar;  /* number of active local variables */
+	u8 nups;  /* number of upvalues */
+	u8 freereg;  /* first free register */
 } FuncState;
 
 
 
 
 /*
- * ** Marks the end of a patch list. It is an invalid value both as an absolute
- * ** address, and as a list link (would link an element to itself).
- * */
+ * Marks the end of a patch list. It is an invalid value both as an absolute
+ * address, and as a list link (would link an element to itself).
+ */
 #define NO_JUMP (-1)
 
 
 /*
- * ** grep "ORDER OPR" if you change these enums  (ORDER OP)
- * */
+ * grep "ORDER OPR" if you change these enums  (ORDER OP)
+ */
 typedef enum BinOpr {
-  OPR_ADD, OPR_SUB, OPR_MUL, OPR_DIV, OPR_MOD, OPR_POW,
-  OPR_CONCAT,
-  OPR_EQ, OPR_LT, OPR_LE,
-  OPR_NE, OPR_GT, OPR_GE,
-  OPR_AND, OPR_OR,
-  OPR_NOBINOPR
+	OPR_ADD, OPR_SUB, OPR_MUL, OPR_DIV, OPR_MOD, OPR_POW,
+	OPR_CONCAT,
+	OPR_EQ, OPR_LT, OPR_LE,
+	OPR_NE, OPR_GT, OPR_GE,
+	OPR_AND, OPR_OR,
+	OPR_NOBINOPR
 } BinOpr;
 
 
