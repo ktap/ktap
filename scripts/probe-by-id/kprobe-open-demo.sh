1d48e67238c774ba53f6039dc1b9f97820ea08af
zhangwei(Jovi) <jovi.zhangwei@gmail.com>
2013-06-18 20:28:58 +0800

    update kprobe and uprobe demo shell script

--- scripts/probe-by-id/kprobe-open-demo.sh
+++ scripts/probe-by-id/kprobe-open-demo.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-#get kernel debuginfo firstly. In Fedora: debuginfo-install kernel
+echo "perf probe -a 'p=do_sys_open'"
+perf probe -a 'p=do_sys_open'
+if [ $? -ne 0 ]; then
+	exit
+fi
 
-echo "perf probe -f do_sys_open"
-perf probe -f do_sys_open
+echo "perf probe -a 'r=do_sys_open%return'"
+perf probe -a 'r=do_sys_open%return'
+if [ $? -ne 0 ]; then
+	exit
+fi
 
-echo "perf probe -f do_sys_open%return"
-perf probe -f do_sys_open%return
-
-id1=`cat /sys/kernel/debug/tracing/events/probe/do_sys_open/id`
-id2=`cat /sys/kernel/debug/tracing/events/probe/do_sys_open_1/id`
+id1=`cat /sys/kernel/debug/tracing/events/probe/p/id`
+id2=`cat /sys/kernel/debug/tracing/events/probe/r/id`
 
 ../../ktap probe_by_id.kp "$id1 $id2"
 
-perf probe --del probe:do_sys_open
-perf probe --del probe:do_sys_open_1
+perf probe --del probe:p
+perf probe --del probe:r
 
--- scripts/probe-by-id/uprobe-malloc-demo.sh
+++ scripts/probe-by-id/uprobe-malloc-demo.sh
@@ -1,31 +1,49 @@
 #!/bin/bash
 
 #make sure your kernel is 3.5 later(support uprobe)
-#uretprobe is not demo in here, because uretprobe is only supported in 3.10 kernel
+#uretprobe is only supported in 3.10 kernel
 
 machine_type=`getconf LONG_BIT`
 
 if [ $machine_type -eq 64 ]; then
-    echo "perf probe -x /lib64/libc.so.6 malloc"
-    perf probe -x /lib64/libc.so.6 malloc
-    echo "perf probe -x /lib64/libc.so.6 free"
-    perf probe -x /lib64/libc.so.6 free
+	LIBC=/lib64/libc.so.6
 elif [ $machine_type -eq 32 ]; then
-    echo "perf probe -x /lib/libc.so.6 malloc"
-    perf probe -x /lib/libc.so.6 malloc
-    echo "perf probe -x /lib/libc.so.6 free"
-    perf probe -x /lib/libc.so.6 free
+	LIBC=/lib/libc.so.6
 fi
 
+echo "perf probe -x $LIBC 'pmalloc=malloc'"
+perf probe -x $LIBC -a 'pmalloc=malloc'
 if [ $? -ne 0 ]; then
 	exit
 fi
 
-id1=`cat /sys/kernel/debug/tracing/events/probe_libc/malloc/id`
-id2=`cat /sys/kernel/debug/tracing/events/probe_libc/free/id`
+echo "perf probe -x $LIBC 'rmalloc=malloc%return'"
+perf probe -x $LIBC 'rmalloc=malloc%return'
+if [ $? -ne 0 ]; then
+	exit
+fi
 
-../../ktap probe_by_id.kp "$id1 $id2"
+echo "perf probe -x $LIBC 'pfree=free'"
+perf probe -x $LIBC 'pfree=free'
+if [ $? -ne 0 ]; then
+	exit
+fi
 
-perf probe --del probe_libc:malloc
-perf probe --del probe_libc:free
+echo "perf probe -x $LIBC 'rfree=free%return'"
+perf probe -x $LIBC 'rfree=free%return'
+if [ $? -ne 0 ]; then
+	exit
+fi
+
+id1=`cat /sys/kernel/debug/tracing/events/probe_libc/pmalloc/id`
+id2=`cat /sys/kernel/debug/tracing/events/probe_libc/rmalloc/id`
+id3=`cat /sys/kernel/debug/tracing/events/probe_libc/pfree/id`
+id4=`cat /sys/kernel/debug/tracing/events/probe_libc/rfree/id`
+
+../../ktap probe_by_id.kp "$id1 $id2 $id3 $id4"
+
+perf probe --del probe_libc:pmalloc
+perf probe --del probe_libc:rmalloc
+perf probe --del probe_libc:pfree
+perf probe --del probe_libc:rfree
 
