ccb59c03b2e312eb82e89df91ba5232d8b8f6424
zhangwei(Jovi) <jovi.zhangwei@gmail.com>
2013-06-24 21:18:58 +0800

    remove CONFIG_KALLSYMS_ALL pre-requirment for running ktap
    Currently ktap tracing interface changed to perf,
    and we don't need to hack global variable in ktapvm,
    so remove CONFIG_KALLSYMS_ALL.

--- interpreter/ktap.c
+++ interpreter/ktap.c
@@ -41,10 +41,6 @@
 #error "Please enable CONFIG_KPROBES before compile ktap"
 #endif
 
-#if !CONFIG_KALLSYMS_ALL
-#error "Please enable CONFIG_KALLSYMS_ALL before compile ktap"
-#endif
-
 #if !CONFIG_RELAY
 #error "Please enable CONFIG_RELAY before compile ktap"
 #endif
