76deaed013a3552d433d468a4ab5ef9d44074cf8
zhangwei(Jovi) <jovi.zhangwei@gmail.com>
2013-07-01 23:14:49 +0800

    complete ktap futex benchmark script

--- test/bench/test.sh
+++ test/bench/test.sh
@@ -1,1 +1,24 @@
-./sembench -t 200 -w 20 -r 30 -o 2
+#!/bin/sh
+
+gcc -o sembench sembench.c -O2 -lpthread
+
+COMMAND="./sembench -t 200 -w 20 -r 30 -o 2"
+echo "Pass 1 without tracing"
+$COMMAND
+echo "Pass 2 without tracing"
+$COMMAND
+echo "Pass 3 without tracing"
+$COMMAND
+
+../../ktap ./futex.kp &
+
+echo "Pass 1 with tracing"
+$COMMAND
+echo "Pass 2 with tracing"
+$COMMAND
+echo "Pass 3 with tracing"
+$COMMAND
+
+pkill ktap
+
+rm -rf ./sembench
