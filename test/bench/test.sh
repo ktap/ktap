1cb654c7a5b219ca7329884e63c6ac331f067bf5
zhangwei(Jovi) <jovi.zhangwei@gmail.com>
2013-07-30 22:59:27 +0800

    update test/bench/test.sh
    Also remove test/bench/futex.kp, instead use oneline script in
    test.sh now.
    
    Signed-off-by: zhangwei(Jovi) <jovi.zhangwei@gmail.com>

--- test/bench/test.sh
+++ test/bench/test.sh
@@ -3,20 +3,22 @@
 gcc -o sembench sembench.c -O2 -lpthread
 
 COMMAND="./sembench -t 200 -w 20 -r 30 -o 2"
-echo "Pass 1 without tracing"
+
+echo -e "\n\t\tPass 1 without tracing"
 $COMMAND
-echo "Pass 2 without tracing"
+echo -e "\n\t\tPass 2 without tracing"
 $COMMAND
-echo "Pass 3 without tracing"
+echo -e "\n\t\tPass 3 without tracing"
 $COMMAND
 
-../../ktap ./futex.kp &
+../../ktap -e 'trace "syscalls:sys_*_futex" function (e) {}' &
 
-echo "Pass 1 with tracing"
+echo -e "\n\t\tPass 1 with tracing"
+echo ""
 $COMMAND
-echo "Pass 2 with tracing"
+echo -e "\n\t\tPass 2 with tracing"
 $COMMAND
-echo "Pass 3 with tracing"
+echo -e "\n\t\tPass 3 with tracing"
 $COMMAND
 
 pkill ktap
