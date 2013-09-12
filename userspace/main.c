e828e14ee8abab0e8ad989a713723b9a92151508
Qingping Hou <dave2008713@gmail.com>
2013-09-12 13:38:10 -0400

    print ktap string properly in dump_function

--- userspace/main.c
+++ userspace/main.c
@@ -237,7 +237,7 @@ static void dump_function(int level, ktap_proto *f)
 			break;
 		case KTAP_TSTRING:
 			printf("\tTSTRING: ");
-			printf("%s\n", getstr(f->k[i].val.gc));
+			printf("%s\n", svalue(&(f->k[i])));
 
 			break;
 		default:
@@ -592,4 +592,5 @@ int main(int argc, char **argv)
 	cleanup_event_resources();
 	return ret;
 }
+
 
