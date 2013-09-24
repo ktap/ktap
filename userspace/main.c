0f7a9c4794a030884d77d6d0948c8b867c2633f7
Alen Skondro <askondro@gmail.com>
2013-09-24 20:19:15 +0200

    remove corrupt comment which resolved in compile error
    Signed-off-by: Alen Skondro <askondro@gmail.com>

--- userspace/main.c
+++ userspace/main.c
@@ -23,24 +23,6 @@
  */
 
 #include <stdio.h>
- * Copyright 2013 The ktap Project Developers.
- * See the COPYRIGHT file at the top-level directory of this distribution.
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms and conditions of the GNU General Public License,
- * version 2, as published by the Free Software Foundation.
- *
- * This program is distributed in the hope it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
- */
-
-#include <stdio.h>
 #include <stdlib.h>
 #include <sched.h>
 #include <string.h>
